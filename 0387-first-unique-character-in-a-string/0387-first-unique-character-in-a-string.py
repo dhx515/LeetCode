47061d0f7345689b1e1e91f1e3d117f8fd77b8ae
Donghyun Kim <dhx515.official@gmail.com>
2024-02-05 20:54:20 +0900

    Time: 145 ms (48.14%), Space: 11.9 MB (98.04%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = Counter(s)
+
+        for i,c in enumerate(s):
+            if count[c]==1:
+                return i
+
+        return -1
