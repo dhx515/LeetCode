a77c4a05fd9b68caed63ce7e7f8bdde57dbb3853
Donghyun Kim <dhx515.official@gmail.com>
2024-03-06 08:37:11 +0900

    Time: 806 ms (61.32%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,5 +1,7 @@
 /* Write your PL/SQL query statement below */
-SELECT class 
-  FROM Courses 
- GROUP BY class 
-HAVING COUNT(class) >= 5
+SELECT A.class
+  FROM (SELECT class, COUNT(student) AS cnt
+          FROM Courses
+         GROUP BY class) A
+ WHERE A.cnt >= 5
+;
