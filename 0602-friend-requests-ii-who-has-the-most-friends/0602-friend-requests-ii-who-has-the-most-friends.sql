dd1eb88790028100fd1b30078b41497edb0da349
Donghyun Kim <dhx515.official@gmail.com>
2024-03-22 23:36:11 +0900

    Time: 1097 ms (34.56%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,29 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+    * 
+FROM
+    (SELECT 
+         A AS id, 
+         SUM(temp) AS num 
+     FROM
+        (
+         SELECT 
+             COUNT(requester_id) temp, 
+             requester_id AS a 
+         FROM 
+             RequestAccepted 
+         GROUP BY requester_id
+
+        union all
+
+         SELECT
+             COUNT(accepter_id) temp,
+             accepter_id AS b 
+         FROM 
+             RequestAccepted 
+         GROUP BY accepter_id
+        )
+    GROUP BY A ORDER BY num DESC) 
+WHERE 
+    ROWNUM = 1
+;
