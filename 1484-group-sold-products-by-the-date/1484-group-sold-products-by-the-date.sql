e662e4ac49ae6a8c63b05cb04f1985f1f519cfaa
Donghyun Kim <dhx515.official@gmail.com>
2024-03-29 18:52:05 +0900

    Time: 1395 ms (20.86%), Space: 0B (100.00%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,15 @@
+/* Write your PL/SQL query statement below */
+SELECT
+    TO_CHAR(sell_date, 'yyyy-mm-dd') AS sell_date,
+    COUNT(DISTINCT(product)) AS num_sold,
+    LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM 
+    (
+        SELECT
+            DISTINCT sell_date,
+            product
+        FROM
+            Activities
+    )
+GROUP BY sell_date
+;
