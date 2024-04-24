d9c277742b7444b606f843a314073108fedcc698
Donghyun Kim <dhx515.official@gmail.com>
2024-04-25 08:21:47 +0900

    Time: 273 ms (79.48%), Space: 14.4 MB (65.66%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for c in s:
+            if c!= '*':
+                stack.append(c)
+            else:
+                stack.pop()
+                
+        return ''.join(stack)
