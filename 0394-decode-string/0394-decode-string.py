b41508cb1405f53b8e7f543895b142e675769623
Donghyun Kim <dhx515.official@gmail.com>
2024-04-27 17:50:06 +0900

    Time: 15 ms (46.42%), Space: 11.8 MB (8.93%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        for i in s:
+            if i != ']':
+                res.append(i)
+            else:
+                substring = ''
+                while res[-1] != '[':
+                    substring = res.pop() + substring
+                res.pop()
+                
+                digit = ''
+                while res and res[-1].isdigit():
+                    digit = res.pop() + digit
+                res.append(int(digit)*substring)
+        
+        return ''.join(res)
+                
+                
+                
+                
+                
+                
+                
+                
+                
