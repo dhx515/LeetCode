2e59b07b9fb94e4c5849483f654b06bca1c9ef02
Donghyun Kim <dhx515.official@gmail.com>
2024-06-09 16:17:02 +0900

    Time: 12 ms (85.24%), Space: 11.8 MB (74.79%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+        
+    def isMirror(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        if not node1 or not node2:
+            return False
+        if node1.val != node2.val:
+            return False
+        return self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
