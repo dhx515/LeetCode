d1d923609dea398443fccfb25db0a9a33c604fd5
Donghyun Kim <dhx515.official@gmail.com>
2024-06-17 09:41:54 +0900

    Time: 562 ms (69.21%), Space: 13.4 MB (91.50%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def nearestExit(self, maze, entr):
+        rows, cols = len(maze), len(maze[0])
+        deq = deque()
+        deq.append([entr[0], entr[1], -1])
+        
+        while deq:
+            r, c, dist = deq.popleft()
+            if not (0 <= r < rows and 0 <= c < cols):
+                if dist > 0:
+                    return dist
+                continue
+            if maze[r][c] == '+':
+                continue
+            
+            maze[r][c] = '+'
+            for _r, _c in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                deq.append([r + _r, c + _c, dist + 1])
+        
+        return -1
