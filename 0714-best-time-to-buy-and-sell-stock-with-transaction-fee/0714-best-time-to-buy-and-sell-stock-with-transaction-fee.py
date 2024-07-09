bb5bc0f1e981430798fdae2119809ea2d73b0fb8
Donghyun Kim <dhx515.official@gmail.com>
2024-07-10 08:26:51 +0900

    Time: 1495 ms (5.08%), Space: 71.1 MB (14.03%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def solve(self, i, bought, fee, n, prices, dp):
+        if i == n:
+            return 0
+        if (i, bought) in dp:
+            return dp[(i, bought)]
+        
+        sell, buy = 0, 0
+        if bought:
+            sell = max(self.solve(i+1, 0, fee, n, prices, dp)+prices[i]-fee, self.solve(i+1, bought, fee, n, prices, dp))
+        else:
+            buy = max(self.solve(i+1, 1, fee, n, prices, dp)-prices[i], self.solve(i+1, bought, fee, n, prices, dp))
+        
+        dp[(i, bought)] = max(sell, buy)
+        return dp[(i, bought)]
+    
+    def maxProfit(self, prices, fee):
+        n = len(prices)
+        dp = dict()
+        return self.solve(0, 0, fee, n, prices, dp)
