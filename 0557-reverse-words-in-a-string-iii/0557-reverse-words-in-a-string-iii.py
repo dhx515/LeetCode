898f95fc0a3316f1eab70938f88b9537a3ff19f3
Donghyun Kim <dhx515.official@gmail.com>
2024-07-13 15:21:45 +0900

    Time: 13 ms (91.63%), Space: 13 MB (13.38%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        wordList = s.split()
-        ans = [word[::-1] for word in wordList]
-        return ' '.join(ans)
+        return ' '.join([word[::-1] for word in s.split()])
