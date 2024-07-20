4d59b4d97c778539072bbefd17874d08bbde296c
Donghyun Kim <dhx515.official@gmail.com>
2024-07-20 23:59:06 +0900

    Time: 19 ms (85.34%), Space: 11.6 MB (79.15%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def shortestToChar(self, s, c):
+        """
+        :type s: str
+        :type c: str
+        :rtype: List[int]
+        """
+        a,n=[],len(s)
+        for i in range(n):
+            if s[i]==c:
+                a.append(i)
+        answer=[]
+        j=0
+        for i in range(n):
+            if s[i]==c:
+                answer.append(0)
+                j+=1
+            elif i<a[0]:
+                answer.append(a[0]-i)
+            elif i>a[-1]:
+                answer.append(i-a[-1])
+            else:
+                answer.append(min((a[j]-i),(i-a[j-1])))
+        return answer
