11680813becd9bb0e528a2347ac90f82a6ee2b90
Donghyun Kim <dhx515.official@gmail.com>
2024-08-21 23:09:13 +0900

    Time: 446 ms (75.68%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,7 +1,12 @@
 /* Write your PL/SQL query statement below */
+/*
 SELECT e.name AS Employee
   FROM Employee e
  WHERE e.salary > (SELECT salary
                      FROM Employee
                     WHERE id = e.managerId)
 ;
+*/
+SELECT A.name AS Employee
+FROM Employee A, Employee B
+WHERE A.managerId = B.id AND A.salary > B.salary
