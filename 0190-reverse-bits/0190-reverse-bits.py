1b0d2a3ed738edaf0439f4147d9ad172030c210b
Donghyun Kim <dhx515.official@gmail.com>
2024-08-28 12:47:24 +0900

    Time: 8 ms (93.30%), Space: 11.7 MB (6.70%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,12 +2,12 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
-        result = 0
-        for i in range(32):
-            # result를 왼쪽으로 1 비트 이동
-            result <<= 1
-            # n의 마지막 비트를 result의 끝에 추가
-            result |= (n & 1)
-            # n을 오른쪽으로 1 비트 이동
-            n >>= 1
-        return result
+        binary_str = bin(n)[2:].zfill(32)
+    
+        # Reverse the binary string
+        reversed_binary_str = binary_str[::-1]
+
+        # Convert the reversed binary string back to an integer
+        reversed_int = int(reversed_binary_str, 2)
+
+        return reversed_int
