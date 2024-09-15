10ab4eb15091a57169fba7acad99a051cf98f484
Donghyun Kim <dhx515.official@gmail.com>
2024-09-15 17:34:24 +0900

    Time: 23 ms (7.82%), Space: 11.7 MB (29.63%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,13 +1,3 @@
 class Solution(object):
     def sumZero(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        ans = []
-        for i in range(1, 1+n//2):
-            ans.extend([i, -1*i])
-        if n%2 == 1:
-            ans.append(0)
-            
-        return ans
+        return list(range(1 - n, n, 2))
