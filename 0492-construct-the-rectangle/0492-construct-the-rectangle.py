2c1fa7cd5acc681bd8232c036bd71e8e7ffdd1b1
Donghyun Kim <dhx515.official@gmail.com>
2024-10-22 22:14:26 +0900

    Time: 0 ms (100.00%), Space: 11.7 MB (24.10%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def constructRectangle(self, area):
+        """
+        :type area: int
+        :rtype: List[int]
+        """
+        W = int(math.sqrt(area))
+        while W > 0:
+            if area%W == 0:
+                L = area // W
+                return [L,W]
+            W -= 1
+            
+        return [area, 1]
